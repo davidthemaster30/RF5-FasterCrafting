b67b99bad87282fc1ba81c6d386367d443ebb8bb
David <davidthemaster30@hotmail.com>
2024-12-06 23:48:57 -0500

    remove bad copy paste of config

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,4 @@ rm -rf bin
 
 dotnet build FasterCrafting.csproj -f net6.0 -c Release
 
-zip -j 'RF5FasterCrafting_v2.1.0.zip' './bin/Release/net6.0/FasterCrafting.dll' './bin/Release/net6.0/FasterCrafting.cfg'
+zip -j 'RF5FasterCrafting_v2.1.0.zip' './bin/Release/net6.0/FasterCrafting.dll'
